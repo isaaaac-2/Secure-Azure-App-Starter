0abe79527161a040c778b1714182fe0aae33ad8d
Isaac <isaaaac-2@users.noreply.github.com>
2026-07-05 15:34:04 +0100

    Update: Fixed the new line error in the keyvault.tf that was flagged[once more]

--- keyvault.tf
+++ keyvault.tf
@@ -5,25 +5,6 @@ resource "azurerm_key_vault" "kv" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete"
-    ]
-  }
-  
-  resource "azurerm_key_vault" "kv" {
-  name                = "secure-app-kv"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-
   soft_delete_retention_days = 7
   purge_protection_enabled   = true
 
@@ -48,12 +29,3 @@ resource "azurerm_key_vault" "kv" {
 }
 
 data "azurerm_client_config" "current" {}
-}
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_key_vault_secret" "example" {
-  name         = "db-password"
-  value        = "super-secret-value-123"
-  key_vault_id = azurerm_key_vault.kv.id
-}
