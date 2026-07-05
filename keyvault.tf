913ca05ad8e8a7daea1a5e2865543efe23cb5efe
Isaac <isaaaac-2@users.noreply.github.com>
2026-07-05 15:24:29 +0100

    Update: Fixed the vulnerabilities Checkov flagged

--- keyvault.tf
+++ keyvault.tf
@@ -15,7 +15,37 @@ resource "azurerm_key_vault" "kv" {
       "Set",
       "Delete"
     ]
+  }resource "azurerm_key_vault" "kv" {
+  name                = "secure-app-kv"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = true
+
+  public_network_access_enabled = false
+
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Deny"
   }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete"
+    ]
+  }
+}
+
+data "azurerm_client_config" "current" {}
 }
 
 data "azurerm_client_config" "current" {}
